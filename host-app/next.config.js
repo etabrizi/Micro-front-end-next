33df0d91b3656cbc5def981fe5687de0d953337f
ls-etabrizi <etabrizi1@gmail.com>
2023-04-18 15:11:16 +0100

    updates

--- host-app/next.config.js
+++ host-app/next.config.js
@@ -7,8 +7,8 @@ module.exports = {
         new NextFederationPlugin({
           name: 'host',
           remotes: {
-            remote: `auth@${process.env.NEXT_AUTH_URL}remote.js`,
-            remote2: `shop@${process.env.NEXT_SHOP_URL}remote.js`
+            remote: `auth@${process.env.NEXT_AUTH_URL}/remote.js`,
+            remote2: `shop@${process.env.NEXT_SHOP_URL}/remote.js`
           },
           filename: 'static/chunks/remoteEntry.js',
         }),

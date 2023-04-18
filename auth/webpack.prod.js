f2d6f1fd93a711a3120a8d0471b45b17f23b50c0
ls-etabrizi <etabrizi1@gmail.com>
2023-04-18 17:54:34 +0100

    updates

---
+++ auth/webpack.prod.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+const { dependencies } = require('./package.json');
+
+module.exports = {
+  entry: './src/index',
+  mode: 'production',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: null,
+  },
+  output: {
+    publicPath: 'https://etabrizi-micro-front-end-auth.netlify.app/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'auth',
+      library: { type: 'var', name: 'auth' },
+      filename: 'remote.js',
+      exposes: {
+        './ReactApp': './src/App',
+      },
+      shared: dependencies
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
